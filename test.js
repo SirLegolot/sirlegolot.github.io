f42ef9925c7f9eca8c190cdc7a050b310ffe1bed
SirLegolot <ngong2020@gmail.com>
2022-03-16 21:03:57 -0400

    test keyboard input hri

---
+++ test.js
@@ -0,0 +1,11 @@
+document.onkeypress = function(e) {
+
+    test.innerHTML = e.type +
+    ' key=' + e.key +
+    ' code=' + e.code +
+    (e.shiftKey ? ' shiftKey' : '') +
+    (e.ctrlKey ? ' ctrlKey' : '') +
+    (e.altKey ? ' altKey' : '') +
+    (e.metaKey ? ' metaKey' : '') +
+    (e.repeat ? ' (repeat)' : '')
+}
