590282708446be5dd106f3d8c51268a6faa4cfc4
SirLegolot <ngong2020@gmail.com>
2022-03-28 12:51:55 -0400

    added some quality of life to rpcs game

--- test_assets/test.js
+++ test_assets/test.js
@@ -25,6 +25,8 @@ function random_hand() {
     } else {
         test.innerHTML = "<img src='test_assets/scissors.png' width='300px' height='300px'/>";
     }
+    text.innerHTML = "Analysing your hand...";
+    loader.style.display = "block";
 }
 
 rockpaperscissors.onended = function() {
@@ -32,9 +34,17 @@ rockpaperscissors.onended = function() {
     shoot.play();
 }
 
-document.onkeypress = function(e) {
-
+async function next_round() {
+    text.innerHTML = "Next round... 3";
+    await sleep(500);
+    text.innerHTML = "Next round... 2";
+    await sleep(500);
+    text.innerHTML = "Next round... 1";
+    await sleep(500);    
     text.innerHTML = "";
+}
+
+document.onkeypress = async function(e) {
     // test.innerHTML = e.type +
     // ' key=' + e.key +
     // ' code=' + e.code +
@@ -44,16 +54,21 @@ document.onkeypress = function(e) {
     // (e.metaKey ? ' metaKey' : '') +
     // (e.repeat ? ' (repeat)' : '');
 
-    if (e.key == 'Enter'){
+    if (e.key == 'Enter'){ 
+        loader.style.display = "none";
+        await next_round();
         test.innerHTML = "<img src='test_assets/rpcs.png' width='300px' height='300px' class='vert-move' />";
         rockpaperscissors.play();
-    } else if (e.key == '1') {
+    } else if (e.key == '1') { 
+        loader.style.display = "none";
         text.innerHTML = "Yes, I win! :)";
         win.play();
     } else if (e.key == '2') {
+        loader.style.display = "none";
         text.innerHTML = "Aw, you win! :(";
         lose.play();
     } else if (e.key == '3') {
+        loader.style.display = "none";
         text.innerHTML = "We have tied this round.";
         tie.play();
     } else if (e.key == 'q') {
